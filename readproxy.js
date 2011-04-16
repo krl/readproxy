c6b9e268830080916736d07845e4a35973ecbcc5
Kristoffer Ström <kristoffer@rymdkoloni.se>
2011-04-16 21:06:48 +0200

    write to stdout option

--- readproxy.js
+++ readproxy.js
@@ -1,25 +1,49 @@
 #!/usr/bin/env node
 
-var fs          = require('fs'),
-    assert      = require('assert'),
-    get         = require('node-get'),
-    readability = require('readability'),
-    spawn       = require('child_process').spawn;
+var fs     = require('fs'),
+    util   = require('util'),
+    assert = require('assert'),
+    get    = require('node-get'),
+    spawn  = require('child_process').spawn;
+
+var directory, url, host
 
 try {
-  var directory = process.argv[2];
-  var url  = process.argv[3];
-  var host = url.split("/")[2];
+  if (process.argv.length == 3) {
+    
+    // hack to stop readability spewing stuff into stdout
+    var oldlog = console.log;
+    console.log = function () {}
+    util.log = function () {}
 
-  assert.ok(directory);
+    url  = process.argv[2];
+    host = url.split("/")[2];    
+  } 
+  else if (process.argv.length == 4) {
+    directory = process.argv[2];
+    url  = process.argv[3];
+    host = url.split("/")[2];
+    assert.ok(directory);
+
+  } else {
+    assert.ok(false);
+  }
+
   assert.ok(url);
   assert.ok(host);
 } catch (err) {
-  console.log("usage: readproxy directory url");
+  console.log("usage:");
+  console.log("readproxy url");
+  console.log(" - writes to stdout");
+  console.log("readproxy directory url");
+  console.log(" - writes to directory (scp or local)");
   return 1;
 }
 
-if (directory[directory.length -1] != "/") {
+var readability = require('readability')
+
+
+if (directory && directory[directory.length -1] != "/") {
   directory += "/";
 }
 
@@ -27,7 +51,6 @@ var get_me = {uri: url,
               headers: { 'Accept-encoding': 'none',
                           'Connection': 'close',
                           'User-Agent': 'Readproxy/0.1'}};
-
 
 new get(get_me).asString(function (err, str) {
   readability.parse(str, url, function(result) {
@@ -38,12 +61,16 @@ new get(get_me).asString(function (err, str) {
       "Subject: "     + result.title + "\n" +
       "From: "        + host + " <readproxy@localhost>\n" +
       "Date: "        + (new Date).toUTCString() + "\n" +
+      "Message-ID: "  + url + "\n" +
       "X-Entry-URL: " + url + "\n\n";
 
     var filename = directory + url.replace(/\//g, "!");
     var content  = header + result.content;    
     
-    if (directory.indexOf(":") == -1) {
+    if (!directory) {
+      oldlog(content)
+    }
+    else if (directory.indexOf(":") == -1) {
       // It's not a scp link -- write directly.
       fs.writeFile(filename, content);
       console.log("Witten to " + filename + " directly.")
